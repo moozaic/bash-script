3e91e58652e3dae37c857b59c0a99d31d10168de
Insan Fajar <insan.fajar@gmail.com>
2016-10-08 20:04:21 +0700

    fixed: file contain spaces

--- bro-compress.sh
+++ bro-compress.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #
-# BROTLI/GZIP: compress CSS and JS files
-# in directory (recursive) for use with Webserver
-#
-# Usage: ./bro-compress.sh [ -c ] [ -d ] directory
-# c : to compress files
-# d : to remove compressed files
-#
-# eg.: bro-compress.sh -c /var/www/mywebsite
+#####################################################
+#                                                   #
+# BROTLI/GZIP: compress CSS and JS files            #
+# in directory (recursive) for use with Webserver   #
+#                                                   #
+# Usage: ./bro-compress.sh [ -c ] [ -d ] directory  #
+# c : to compress files                             #
+# d : to remove compressed files                    #
+#                                                   #
+# eg.: bro-compress.sh -c /var/www/mywebsite        #
+#                                                   #
+# FIX: file contain spaces                          #
+#                                                   #
+#####################################################
+
 
 ###################
 #  CONFIGURATION  #
@@ -20,53 +27,50 @@ USE_BRO=1
 USE_GZP=1
 
 # Brotli executable (path)
-BRO=/opt/brotli/bin/bro
+BRO=/opt/brotli/tools/bro
 
 # Nginx/Apache/Webserver user
-WEBUSER=www-data
-WEBGROUP=www-data
+WEBUSER=insan
+WEBGROUP=insan
 
-###################
-#     ROUTINE     #
-###################
+
+#############
+#  ROUTINE  #
+#############
 
 NAME="bro-compress.sh"
-BLU='\033[1;34m'
 RED='\033[1;31m'
 GRN='\033[1;32m'
 NC='\033[0m'
 
-# compress using brotli and gzip
+
 bro_action() {
   echo -e "Compressing ${GRN}$1${NC}"
   if [ "$USE_BRO" -gt 0 ]; then
-    $BRO --input $1 --force --output $1.br;
-    chmod 0644 $1.br;
+    $BRO --input "$1" --force --output "$1.br";
+    chmod 0644 "$1.br";
   fi
-
   if [ "$USE_GZP" -gt 0 ]; then
-    gzip -9 $1 -c > $1.gz;
-    chmod 0644 $1.gz;
+    gzip -9 "$1" -c > "$1.gz";
+    chmod 0644 "$1.gz";
   fi
-  chown $WEBUSER:$WEBGROUP $1.br $1.gz;
+  chown $WEBUSER:$WEBGROUP "$1.br" "$1.gz";
 }
 
-# find CSS/JS files in directory and sub-directory
 do_compress() {
-  echo -e "\nProcessing CSS : ${BLU}$1${NC}"
-  for x in `find $1 -type f -name '*.css'`; do bro_action ${x}; done
+  echo -e "\nProcessing CSS : ${RED}$1${NC}"
+  find $1 -type f -iname "*.css" | while read -r x; do bro_action "${x}" ; done
   echo -e "Done.\n"
 
-  echo -e "Processing JS : ${BLU}$1${NC}"
-  for x in `find $1 -type f -name '*.js'`; do bro_action ${x}; done
+  echo -e "Processing JS : ${RED}$1${NC}"
+  find $1 -type f -iname "*.js" | while read -r x; do bro_action "${x}" ; done
   echo -e "Done.\n"
 }
 
-# delete compressed files
 remove_compress() {
-  echo -e "\nRemoving compressed files in ${BLU}$1${NC}"
-  for x in `find $1 -type f -name '*.gz' -o -name '*.br'`; do
-    rm -f ${x}
+  echo -e "\nRemoving compressed files in ${RED}$1${NC}"
+  find $1 -type f -iname "*.gz" -o -iname "*.br" | while read -r x; do
+    rm -f "${x}"
     echo -e "File ${GRN}${x}${NC}... ${RED}Deleted${NC}"
   done
 }
@@ -83,13 +87,13 @@ print_usage() {
   echo -e "\n Usage: $NAME [ -c ] [ -d ] directory\n Options: \n   c : to compress files\n   d : to remove compressed files\n"
 }
 
-if [ $USE_BRO -eq 0 ] && [ $USE_GZP -eq 0 ]; then
-  echo "Edit configuration in $NAME"
+if [ $# -ne 2 ]; then
+  print_usage
   exit 3
 fi
 
-if [ $# -ne 2 ]; then
-  print_usage
+if [[ $USE_BRO -eq 0 ]] && [[ $USE_GZP -eq 0 ]]; then
+  echo "Edit configuration in $NAME"
   exit 3
 fi
 
